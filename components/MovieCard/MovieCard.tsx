17e4e14c92c3ebfb02fc11b850d37a3e6b502070
Tendai Karuwo <tendaikaruwo96@gmail.com>
2023-01-11 11:57:45 +0000

    placed bookmark

--- components/MovieCard/MovieCard.tsx
+++ components/MovieCard/MovieCard.tsx
@@ -12,7 +12,7 @@ import Image from 'next/image';
 export default function MovieCard() {
   return (
     <div className="w-[10vw]  relative bg-zinc-50">
-        <Image className="z-0" src="/bookmark.svg" alt="bookmark icon" width={20} height={20}/>
+        <Image className="abosolute z-10 absolute left-0 top-0" src="/bookmark.svg" alt="bookmark icon" width={20} height={20}/>
         <Image className="relativeobject-cover object-center" width={200} height={280} src="/AvatarImage.jpg" alt="avatar movie poster"/>
         <p>⭐6</p>
       <p>Avatar: The Way of Water</p>
