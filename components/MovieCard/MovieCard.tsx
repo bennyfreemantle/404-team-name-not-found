aa50c9fcbaf233aef5cc10d4692166c65616bc31
lani-irving <laniirving@googlemail.com>
2023-01-11 14:48:37 +0000

    more progress with movie card

--- components/MovieCard/MovieCard.tsx
+++ components/MovieCard/MovieCard.tsx
@@ -10,11 +10,15 @@ import Image from 'next/image';
 
 export default function MovieCard() {
   return (
-    <div className="w-[10vw]  relative bg-zinc-50">
-        <Image className="abosolute z-10 absolute left-0 top-0" src="/bookmark.svg" alt="bookmark icon" width={20} height={20}/>
-        <Image className="relativeobject-cover object-center" width={200} height={280} src="/AvatarImage.jpg" alt="avatar movie poster"/>
-        <p className="text-slate-700">⭐6</p>
-      <p className="text-slate-700">Avatar: The Way of Water</p>
+    <div className="w-[10vw]  relative bg-zinc-50 rounded-md overflow-hidden m-4">
+        <Image className="absolute z-10 left-0 top-0" src="/bookmark.svg" alt="bookmark icon" width={40} height={40}/>
+        <Image className="h-200 w-full relative object-cover object-top" width={200} height={280} src="/AvatarImage.jpg" alt="avatar movie poster"/>
+        
+      <p className="text-slate-700 pl-2">Avatar: The Way of Water</p>
+      <div className="inline-block space-x-2 bg-slate-800 w-full">
+      <p className="text-amber-50 text-md inline-block font-bold">⭐6</p>
+        <p className="text-amber-50 text-sm inline-block italic">Recommended by Ben!👍</p>
+      </div>
   
     </div>
   )
