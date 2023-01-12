264317d8bc0fb4d5535741ea963c0269f59e970e
Ben Freemantle <52469359+bennyfreemantle@users.noreply.github.com>
2023-01-12 09:49:24 +0000

    moved footer outside of the parent div so it spans the full width of the page (#7)

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -6,14 +6,12 @@ import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useState } from "react";
 
 export default function Profile() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  
-  const session = useSession();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  
+  const session = useSession();
 
   return (
     <div className="bg-slate-800">
@@ -25,47 +23,65 @@ export default function Profile() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <Header />
-      </div>      
+      </div>
       <div className="flex flex-col min-h-screen container mx-auto my-0 p-3">
         <main className="flex gap-4 flex-1 flex-col text-amber-50">
           <div>
             <h1 className="text-2xl font-bold">Profile settings</h1>
             <p>Change your account settings here</p>
           </div>
-        <div className="flex justify-center md:justify-start">
-          <Image alt="profile pic placeholder" src='./person.svg' width={50} height={50}/>
-        </div>
-        <form className="flex flex-col gap-4 md:w-1/2">
-          <div className="flex flex-col md:flex-row md:justify-between">
-            <label>Your name:</label>
-            <input className="w-2/3" type="text" value={name} onChange={(e) => 
-              setName(e.target.value)}>
-            </input>
-          </div>  
-          <div className="flex flex-col md:flex-row md:justify-between">
-            <label>Your Username:</label>
-            <input className="w-2/3" type="text" value={username} onChange={(e) => 
-              setUsername(e.target.value)}>
-            </input>
-          </div>  
-          <div className="flex flex-col md:flex-row md:justify-between">
-            <label>Your Email:</label>
-            <input className="w-2/3" type="email" value={email} onChange={(e) => 
-              setEmail(e.target.value)}>
-            </input>
-          </div> 
-          <div className="flex flex-col md:flex-row md:justify-between">
-            <label>Your Password:</label>
-            <input className="w-2/3" type="password" value={password} onChange={(e) => 
-              setPassword(e.target.value)}>
-            </input>
+          <div className="flex justify-center md:justify-start">
+            <Image
+              alt="profile pic placeholder"
+              src="./person.svg"
+              width={50}
+              height={50}
+            />
           </div>
-          <button className="rounded-xl bg-red-400 text-amber-50 p-1 text-lg md:w-32">Save</button>
-        </form>  
-            
+          <form className="flex flex-col gap-4 md:w-1/2">
+            <div className="flex flex-col md:flex-row md:justify-between">
+              <label>Your name:</label>
+              <input
+                className="w-2/3"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></input>
+            </div>
+            <div className="flex flex-col md:flex-row md:justify-between">
+              <label>Your Username:</label>
+              <input
+                className="w-2/3"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              ></input>
+            </div>
+            <div className="flex flex-col md:flex-row md:justify-between">
+              <label>Your Email:</label>
+              <input
+                className="w-2/3"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+            </div>
+            <div className="flex flex-col md:flex-row md:justify-between">
+              <label>Your Password:</label>
+              <input
+                className="w-2/3"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
+            <button className="rounded-xl bg-red-400 text-amber-50 p-1 text-lg md:w-32">
+              Save
+            </button>
+          </form>
         </main>
-        <Footer />
       </div>
+      <Footer />
     </div>
   );
 }
