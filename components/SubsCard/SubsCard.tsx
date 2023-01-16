b2c4062cf165f85baa6b243ab90dcde04a664957
lani-irving <laniirving@googlemail.com>
2023-01-16 16:02:07 +0000

    worked on styling of subs card

--- components/SubsCard/SubsCard.tsx
+++ components/SubsCard/SubsCard.tsx
@@ -8,17 +8,19 @@ type SubsCardProp = {
 
 export default function SubsCard({ title, price, detail }: SubsCardProp) {
   return (
-    <div  className=" bg-slate-900 text-white p-8">
-
-		<div>
-      <h1>{title}</h1>
-      <h2>{price}</h2>
-      <ul className="list-disc">
-        {detail.map((item, index) => {
-          return <li key={index}>{item}</li>;
-        })}
-      </ul>
-	  </div>
+    <div className="m-2 w-80 h-96 bg-slate-900 text-white p-8 rounded-lg">
+      <div>
+        {/* need to position radio button closer to top corner */}
+        <input type="radio" name="subscription" className="absolute"></input>
+        <h1 className="text-3xl text-center">{title}</h1>
+        {/* per month text needs to be smaller than price */}
+        <h2 className="text-2xl text-center">{price}/month</h2>
+        <ul className="list-disc p-6">
+          {detail.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import Carousel from "../components/Carousel/Carousel";
-import Subscription from "../components/Subscription/subscription";
+import Subscription from "../components/Subscription/Subscription";
 
 const movies = [{ name: "hehe" }, { name: "ehehe" }];
 export default function Home() {
@@ -25,19 +25,12 @@ export default function Home() {
 
       <Carousel />
       <div className="bg-slate-800">
-        <div className="flex flex-col min-h-screen container mx-auto my-0 p-3">
-          <main className="flex-1 text-amber-50">
-            <p>This is the rest of the main content</p>
-            {session && <p>User logged in {session.user.email}</p>}
-          </main>
-        </div>
+        
+   
+      <div className="flex flex-col items-center min-h-screen container mx-auto my-0 p-3">
       <Subscription />
-      <div className="flex flex-col min-h-screen container mx-auto my-0 p-3">
-        <main className="flex-1 text-amber-50">
-          {session && <p>User logged in {session.user.email}</p>}
-        </main>
       </div>
-
+      </div>
       <Footer />
     </div>
   );
