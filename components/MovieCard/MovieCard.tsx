9b5153c86c7f777da29a129fb11ad2bdc57b6a1d
Ben Freemantle <52469359+bennyfreemantle@users.noreply.github.com>
2023-01-17 09:34:06 +0000

    UI movierecs (#14)
    * fixed movie recs styling
    
    * added external link to the movie for more info when clicked on - add ui hover effect
    
    * WIP - responsive design styles to the cards
    
    * fixed next Image warnings

--- components/MovieCard/MovieCard.tsx
+++ components/MovieCard/MovieCard.tsx
@@ -2,67 +2,65 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import moviedb from "../../utils/moviedbclient";
 import { MovieResult } from "moviedb-promise/dist/request-types";
-
-
-
+import Link from "next/link";
 
 export default function MovieCard() {
-  const [movies, setMovies] = useState<MovieResult[]>()
-  useEffect(
-    () => {
-      async function List() {
-        const response = await moviedb.moviePopular()
-        setMovies(response.results)
-      console.log(response)}
-    List()}
-    
-  ,[])
+  const [movies, setMovies] = useState<MovieResult[]>();
+  useEffect(() => {
+    async function List() {
+      const response = await moviedb.moviePopular();
+      setMovies(response.results);
+    }
+    List();
+  }, []);
 
   return (
     <div className="w-full flex flex-wrap relative gap-y-8 gap-x-4 justify-evenly bg-slate-700 m-4">
       {movies?.map((movie: MovieResult) => {
         return (
-          <div className="relative drop-shadow-xl rounded-md overflow-hidden bg-amber-50" key={movie.id}>
-            <Image
-              className="absolute z-10 left-0 top-0"
-              src="/bookmark.svg"
-              alt="bookmark icon"
-              width={40}
-              height={40}
-            />
-            <div className="h-[500px] relative">
-            <Image
-              className="w-full relative object-cover object-center"
-              // width={200}
-              // height={280}
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              fill
-              alt="movie poster"
-              key={movie.id}
-            />
-            </div>
+          <a
+            key={movie.id}
+            target={"_blank"}
+            href={`https://www.themoviedb.org/movie/${movie.id}`}
+            rel="noreferrer"
+          >
+            <div className="relative h-[500px] flex flex-col w-44 drop-shadow-xl rounded-md overflow-hidden bg-amber-50 hover:opacity-70 hover:cursor-pointer md:w-56 xl:w-80">
+              <Image
+                className="absolute z-10 left-0 top-0"
+                src="/bookmark.svg"
+                alt="bookmark icon"
+                width={40}
+                height={40}
+                style={{ width: "auto", height: "auto" }}
+              />
+              <div className="relative h-full">
+                <Image
+                  className="w-full relative object-cover object-center aspect-square"
+                  // width={200}
+                  // height={280}
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  fill
+                  priority
+                  sizes="50vw"
+                  alt="movie poster"
+                />
+              </div>
 
-            <p
-              className="text-slate-700 pl-2 text-xl text-center py-4"
-              key={movie.id}
-            >
-              {movie.title}
-            </p>
-            <div className="inline-block space-x-2 bg-slate-800 w-full py-2">
-              <p
-                className="text-amber-50  inline-block font-bold pl-7 text-xl"
-                key={movie.id}
-              >
-                ⭐{movie.vote_average}
+              <p className="flex-1 flex items-center justify-center text-slate-700 text-lg text-center p-2">
+                {movie.title}
               </p>
-              <p className="text-amber-50  inline-block text-l italic pr-5">
-                Recommended by Ben!👍
-              </p>
+              <div className="flex items-center gap-2 bg-slate-800 w-full p-2">
+                <p className="text-amber-50 font-bold text-sm">
+                  ⭐{movie.vote_average}
+                </p>
+                <p className="text-amber-50  text-sm italic md:text-lg">
+                  Recommended by Ben!👍
+                </p>
+              </div>
             </div>
-          </div>
+          </a>
         );
       })}
     </div>
   );
 }
-
