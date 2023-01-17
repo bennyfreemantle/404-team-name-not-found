d2306f38cad518c36a2a5d09fc61e21914aae479
diinif <fdiini91@gmail.com>
2023-01-17 10:20:53 +0000

    added radio button which when selected adds a border to the card

--- components/SubsCard/SubsCard.tsx
+++ components/SubsCard/SubsCard.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 
+
+
 type SubsCardProp = {
   title: string;
   price: string;
   detail: string[];
+  className: string,
+  onClick: () => void
 };
 
-export default function SubsCard({ title, price, detail }: SubsCardProp) {
+
+export default function SubsCard({ title, price, detail, className, onClick }: SubsCardProp) {
+
   return (
-    <div className="m-2 w-80 h-96 bg-slate-900 text-white p-8 rounded-lg">
-      <div>
+    <div onClick={onClick} className={`m-2 w-80 h-96 bg-slate-900 text-white p-8 rounded-lg ${className}`}>
+      <div >
         {/* need to position radio button closer to top corner */}
-        <input type="radio" name="subscription" className="absolute"></input>
+        <input checked type="radio" name="subscription" className="absolute"></input>
         <h1 className="text-3xl text-center">{title}</h1>
         {/* per month text needs to be smaller than price */}
         <h2 className="text-2xl text-center">{price}/month</h2>
@@ -20,6 +26,7 @@ export default function SubsCard({ title, price, detail }: SubsCardProp) {
             return <li key={index}>{item}</li>;
           })}
         </ul>
+    
       </div>
     </div>
   );
--- components/Subscription/Subscription.tsx
+++ components/Subscription/Subscription.tsx
@@ -1,7 +1,20 @@
 import React from "react"
 import SubsCard from "../SubsCard/SubsCard";
+import { useState } from "react";
 
 export default function Subscription() {
+	const [checked, setChecked] = useState(false)
+	const [checked2, setChecked2] = useState(false)
+	const [checked3, setChecked3] = useState(false)
+
+
+
+	const checkedStyle = checked ? "border border-red-400": "";
+	const checkedStyle2 = checked2 ? "border border-red-400": "";
+	const checkedStyle3 = checked3 ? "border border-red-400": "";
+
+
+	
 	const freeDetail = ["Recommend 30 Movies & Shows", "Web, IOS & Android app", "Upgrade at any time"]
 	const criticDetail = ["Recommend 100 Movies & Shows", "Web, IOS & Android app", "Private film critic chat / forum", "Upgrade at any time"]
 	const directorDetail = ["Recommend ∞ Movies & Shows", "Web, IOS & Android app", "Private film critic chat / forum", "Be recognised as a top supporter ", "Invited to exclusive prize draws within the movie industry"]
@@ -11,9 +24,9 @@ export default function Subscription() {
 		<div>
 			<p className="text-white text-center">SUBSCRIPTION</p>
 			<div className="flex gap-8 flex-col md:flex-row">
-			<SubsCard title="Free" price="£0.00" detail={freeDetail} />
-			<SubsCard title="Film Critic" price="£1.99" detail={criticDetail}/>
-			<SubsCard title="Film Director" price="£4.99" detail={directorDetail}/>
+			<SubsCard onClick={()=> setChecked(!checked)} className={checkedStyle}  title="Free" price="£0.00" detail={freeDetail} />
+			<SubsCard onClick={()=> setChecked2(!checked2)} className={checkedStyle2} title="Film Critic" price="£1.99" detail={criticDetail}/>
+			<SubsCard onClick={()=> setChecked3(!checked3)} className={checkedStyle3} title="Film Director" price="£4.99" detail={directorDetail}/>
 			</div>
 		</div>
 	  </>
