3c663a1d5e9b599c5c60c50809fa20bb55e1806d
bennyfreemantle <bennyfreemantle@protonmail.com>
2023-01-17 12:20:51 +0000

    user profile page updates username field for user

---
+++ types/supabase.ts
@@ -0,0 +1,69 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      movies: {
+        Row: {
+          id: number
+          image_url: string | null
+          movie_id: number | null
+          title: string | null
+        }
+        Insert: {
+          id?: number
+          image_url?: string | null
+          movie_id?: number | null
+          title?: string | null
+        }
+        Update: {
+          id?: number
+          image_url?: string | null
+          movie_id?: number | null
+          title?: string | null
+        }
+      }
+      profiles: {
+        Row: {
+          avatar_url: string | null
+          full_name: string | null
+          id: string
+          updated_at: string | null
+          username: string | null
+          website: string | null
+        }
+        Insert: {
+          avatar_url?: string | null
+          full_name?: string | null
+          id: string
+          updated_at?: string | null
+          username?: string | null
+          website?: string | null
+        }
+        Update: {
+          avatar_url?: string | null
+          full_name?: string | null
+          id?: string
+          updated_at?: string | null
+          username?: string | null
+          website?: string | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+  }
+}
