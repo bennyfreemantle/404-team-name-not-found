c2aae9af24488e0bee5fda6042131eddca15f274
lani-irving <laniirving@googlemail.com>
2023-01-18 14:26:58 +0000

    delete function working

--- components/RecommendedMovieCard/RecommendedMovieCard.tsx
+++ components/RecommendedMovieCard/RecommendedMovieCard.tsx
@@ -7,6 +7,7 @@ import {
   useSupabaseClient,
   useUser,
 } from "@supabase/auth-helpers-react";
+import { MdDelete } from "react-icons/md"
 
 type Movies = Database["public"]["Tables"]["movies"]["Row"];
 
@@ -23,7 +24,7 @@ export default function RecommendedMovieCard({
 
   useEffect(() => {
     getMovies();
-  }, [user]);
+  }, [user, handleDelete]);
 
   async function getMovies() {
     try {
@@ -50,19 +51,40 @@ export default function RecommendedMovieCard({
     }
   }
 
+  async function handleDelete(movie: Movies) {
+    if (!user) return;
+    try {
+      const { data: movieData, error, status } = await supabase
+        .from("movies")
+        .delete()
+        .eq("id", movie.id)
+        if(error && status !== 406){
+          throw error
+        }
+      console.log(movieData);
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(movie);
+  }
+  
+
   // TODO : Generate and save the base url for the card link /tv or /movie
 
   return (
     <div className="w-full flex flex-wrap relative gap-y-8 gap-x-4 justify-evenly bg-slate-700 m-4">
       {movies?.map((movie) => {
         return (
-          <a
-            key={movie.id}
-            target={"_blank"}
-            href={`https://www.themoviedb.org/movie/${movie.movie_id}`}
-            rel="noreferrer"
-          >
-            <div className="relative h-[500px] flex flex-col w-44 drop-shadow-xl rounded-md overflow-hidden bg-amber-50 hover:opacity-70 hover:cursor-pointer md:w-56 xl:w-80">
+          // <a
+          //   key={movie.id}
+          //   target={"_blank"}
+          //   href={`https://www.themoviedb.org/movie/${movie.movie_id}`}
+          //   rel="noreferrer"
+          // >
+            <div className="relative h-[500px] flex flex-col w-44 drop-shadow-xl rounded-md overflow-hidden bg-amber-50 hover:opacity-70 hover:cursor-pointer md:w-56 xl:w-80" key={movie.id}>
+              
+              <MdDelete className="absolute z-10 left-0 top-0 text-slate-900" size={50} onClick={()=> handleDelete(movie)}/>
+            
               <div className="relative h-full">
                 <Image
                   className="w-full relative object-cover object-center aspect-square"
@@ -78,7 +100,7 @@ export default function RecommendedMovieCard({
                 {movie.title}
               </p>
             </div>
-          </a>
+          // </a>
         );
       })}
     </div>
--- types/supabase.ts
+++ types/supabase.ts
@@ -1,4 +1,4 @@
-export type Json =
+npmexport type Json =
   | string
   | number
   | boolean
