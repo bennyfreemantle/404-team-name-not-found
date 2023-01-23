04a54ebe975a4acccd3c2635fa4f494e2c50bd22
bennyfreemantle <bennyfreemantle@protonmail.com>
2023-01-23 09:41:10 +0000

    Added carousel UX feedback when clicking on film - todo: add to database on click

--- components/CarouselContainer/CarouselContainer.tsx
+++ components/CarouselContainer/CarouselContainer.tsx
@@ -5,6 +5,12 @@ import { useState, useEffect } from "react";
 import moviedb from "../../utils/moviedbclient";
 import Image from "next/image";
 
+import Swal from "sweetalert2";
+
+const swal = Swal.mixin({
+  buttonsStyling: true,
+});
+
 const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
@@ -36,6 +42,36 @@ export function CarouselContainer() {
     List();
   }, []);
 
+  function handleMovieClick(movie: MovieResult) {
+    // add an alert for the user to add the move
+    // add movie to our db
+    swal
+      .fire({
+        title: "Add this movie ?",
+        text: "You can view added movies on your recommended movie page",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonText: "Yes, add movie",
+        cancelButtonText: "No, cancel",
+        reverseButtons: true,
+        background: "#fffbeb",
+        cancelButtonColor: "#F87171",
+        confirmButtonColor: "#1E293B",
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          swal.fire({
+            title: "Added",
+            text: "Your movie has been added",
+            background: "#fffbeb",
+            confirmButtonColor: "#1E293B",
+          });
+
+          // add to db
+        }
+      });
+  }
+
   return (
     <div className="m-4">
       <h1 className="mb-4 text-amber-50 text-3xl p-4">Top Rated Movies</h1>
@@ -49,6 +85,7 @@ export function CarouselContainer() {
           movies.map((movie) => {
             return (
               <Image
+                onClick={() => handleMovieClick(movie)}
                 key={movie.id}
                 draggable="false"
                 alt="movie"
