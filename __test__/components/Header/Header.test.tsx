f8e538f461baac0d7b063be28fad61ea3080f1dd
bennyfreemantle <bennyfreemantle@protonmail.com>
2023-01-24 10:54:39 +0000

    fixed router mock function

--- __test__/components/Header/Header.test.tsx
+++ __test__/components/Header/Header.test.tsx
@@ -3,14 +3,15 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Header from "@/components/Header/Header";
 
-describe ("Header component", ()=> {
-    it("Should render properly", ()=> {
-        render(<Header/>)
+jest.mock("next/router", () => require("next-router-mock"));
 
-        const logoText = screen.getByTestId("logo-text");
-        const projectName = "CineMate";
+describe("Header component", () => {
+  it("Should render our logo text in the header", () => {
+    render(<Header />);
 
-        expect(logoText).toHaveTextContent(projectName)
+    const logoText = screen.getByTestId("logo-text");
+    const projectName = "CineMate";
 
-    })
-})
+    expect(logoText).toHaveTextContent(projectName);
+  });
+});
